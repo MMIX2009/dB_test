e6069193fa3d309221559bbc7248f907a1bc9cc6
MMIX2009 <btshilombo@comcast.net>
2023-06-02 15:45:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 
 st.title('This Web app publication test ')
 st.button('Click me')
-st.text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt dolor id orci pretium, ut dictum urna viverra. Curabitur accumsan laoreet eros, at facilisis magna rhoncus eget. In varius odio vel nisl mollis, in auctor enim porta. Nunc maximus metus nisi. Aliquam vitae dui sed nibh varius bibendum eget eget sem. Pellentesque purus nibh, maximus sed sapien sed, commodo sagittis justo. Morbi eu diam purus. Cras vestibulum enim nisi. Mauris at ligula a est vehicula venenatis. Mauris ac egestas sapien, ut tempus arcu.
-
-Integer placerat elit venenatis gravida luctus. Phasellus pellentesque tincidunt ligula at porttitor. Vivamus sed velit ac justo condimentum eleifend et in nisl. Sed a consectetur dui, in aliquet lectus. Suspendisse interdum diam nec elit dignissim cursus. Mauris finibus ex dolor, vitae scelerisque magna lacinia ac. Nulla vel bibendum magna. Nullam at pharetra diam. Sed convallis risus maximus turpis tristique, in pharetra quam pretium. Vivamus sit amet tortor in enim gravida sollicitudin. Maecenas mattis nunc ac libero pellentesque vestibulum. Nullam a ligula pharetra, fringilla enim eu, fermentum odio. Nulla sollicitudin mauris in dui condimentum, ut ornare ex elementum. Quisque bibendum commodo turpis, eu interdum ex semper et.
-
-Etiam aliquet eu elit vitae bibendum. Praesent gravida imperdiet finibus. Donec ac eros non purus pharetra euismod. Donec vitae mi sollicitudin, posuere turpis vel, rutrum nunc. Integer vitae laoreet erat. Morbi vehicula volutpat condimentum. Pellentesque venenatis nibh eleifend, fringilla dolor blandit, ultrices nisl.
-
-Integer malesuada magna sit amet purus tempus auctor a sed est. Suspendisse efficitur lectus ac justo accumsan, sit amet viverra velit molestie. Duis vitae posuere mi. Vivamus id laoreet felis. Suspendisse id laoreet nibh. Integer accumsan non ante ut vulputate. Mauris lobortis molestie mauris a rhoncus. Fusce sit amet purus nec quam posuere efficitur. Curabitur vitae pellentesque diam.
-
-Donec faucibus sit amet justo id interdum. Suspendisse felis felis, iaculis vel interdum et, interdum sed nisi. Nulla facilisi. Vivamus convallis orci vitae lectus sagittis, eget venenatis neque porttitor. Vestibulum tincidunt nisl non neque iaculis, ac bibendum mauris ultrices. Sed euismod ultrices ante accumsan varius. Maecenas lorem erat, pharetra ac scelerisque vitae, bibendum nec velit. Vestibulum quis elit vulputate orci hendrerit pellentesque at in mauris.
-')
+st.text('Lorem ipsum dolor sit amet, consectetur adipisci')
 
 
